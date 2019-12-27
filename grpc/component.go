3c4b4ca90aeee620014e5a4b2f15462ef2b57f13
Sotirios Mantziaris <smantziaris@gmail.com>
2019-12-27 18:16:12 +0200

    Adding creation
    Signed-off-by: Sotirios Mantziaris <smantziaris@gmail.com>

--- grpc/component.go
+++ grpc/component.go
@@ -2,16 +2,17 @@ package grpc
 
 import (
 	"context"
-	"errors"
 	"log"
 	"net"
 
+	"github.com/beatlabs/patron/errors"
 	"google.golang.org/grpc"
 )
 
 // Component of a gRPC service.
 type Component struct {
 	port string
+	srv  *grpc.Server
 }
 
 // Run the gRPC service.
@@ -21,17 +22,16 @@ func (c *Component) Run(ctx context.Context) error {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
 	//s.RegisterService(sd *grpc.ServiceDesc, ss interface{})
 
 	go func() {
 		for range ctx.Done() {
-			s.GracefulStop()
+			c.srv.GracefulStop()
 			break
 		}
 	}()
 
-	return s.Serve(lis)
+	return c.srv.Serve(lis)
 }
 
 // Builder pattern for our gRPC service.
@@ -48,4 +48,14 @@ func New(port string) *Builder {
 	}
 	b.port = port
 	return b
+}
+
+// Create the gRPC component.
+func (b *Builder) Create() (*Component, error) {
+	if len(b.errors) != 0 {
+		return nil, errors.Aggregate(b.errors...)
+	}
+	// TODO: create grpc server and options...
+	srv := grpc.NewServer()
+	return &Component{port: b.port, srv: srv}, nil
 }
