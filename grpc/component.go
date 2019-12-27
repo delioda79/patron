1d7f30d93dbdbc03c806cfdd4f9ad05d8183654e
Sotirios Mantziaris <smantziaris@gmail.com>
2019-12-27 20:33:34 +0200

    Builder implementation
    Signed-off-by: Sotirios Mantziaris <smantziaris@gmail.com>

--- grpc/component.go
+++ grpc/component.go
@@ -22,8 +22,6 @@ func (c *Component) Run(ctx context.Context) error {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	//s.RegisterService(sd *grpc.ServiceDesc, ss interface{})
-
 	go func() {
 		for range ctx.Done() {
 			c.srv.GracefulStop()
@@ -34,10 +32,17 @@ func (c *Component) Run(ctx context.Context) error {
 	return c.srv.Serve(lis)
 }
 
+type definition struct {
+	description *grpc.ServiceDesc
+	server      interface{}
+}
+
 // Builder pattern for our gRPC service.
 type Builder struct {
-	port   string
-	errors []error
+	port          string
+	serverOptions []grpc.ServerOption
+	definitions   []definition
+	errors        []error
 }
 
 // New builder.
@@ -50,12 +55,37 @@ func New(port string) *Builder {
 	return b
 }
 
+// WithOptions allows gRPC server options to be set.
+func (b *Builder) WithOptions(oo ...grpc.ServerOption) *Builder {
+	if len(b.errors) != 0 {
+		return b
+	}
+	b.serverOptions = append(b.serverOptions, oo...)
+	return b
+}
+
+// WithDefinition allows
+func (b *Builder) WithDefinition(description *grpc.ServiceDesc, server interface{}) *Builder {
+	if len(b.errors) != 0 {
+		return b
+	}
+	b.definitions = append(b.definitions, definition{description: description, server: server})
+	return b
+}
+
 // Create the gRPC component.
 func (b *Builder) Create() (*Component, error) {
 	if len(b.errors) != 0 {
 		return nil, errors.Aggregate(b.errors...)
 	}
-	// TODO: create grpc server and options...
-	srv := grpc.NewServer()
-	return &Component{port: b.port, srv: srv}, nil
+	srv := grpc.NewServer(b.serverOptions...)
+
+	for _, def := range b.definitions {
+		srv.RegisterService(def.description, def.server)
+	}
+
+	return &Component{
+		port: b.port,
+		srv:  srv,
+	}, nil
 }
